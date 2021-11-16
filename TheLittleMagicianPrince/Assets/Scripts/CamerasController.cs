0ccb7abbf1edd03c8006826f2633bd5ab25c7fb2
N-S-LEHCIM <michel.ilva.akassugui@hotmail.com>
2021-11-16 18:00:45 -0300

    Actualizar

--- TheLittleMagicianPrince/Assets/Scripts/CamerasController.cs
+++ TheLittleMagicianPrince/Assets/Scripts/CamerasController.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class CamerasController : MonoBehaviour
 {
-   
-    enum camera { cam1,cam2 };
-    public GameObject[] cameras;
-    private camera CAM;
 
+    public GameObject[] cameras;
 
     // Start is called before the first frame update
     void Start()
@@ -19,22 +16,17 @@ public class CamerasController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool cam1 = Input.GetKeyDown(KeyCode.F1);
-        bool cam2 = Input.GetKeyDown(KeyCode.F2);
-
-        switch (CAM)
+        if (Input.GetKeyDown(KeyCode.F1))
         {
-            case camera.cam1:
-                cameras[0].SetActive(true);
-                cameras[1].SetActive(false);
-                break;
-            case camera.cam2:
-                cameras[0].SetActive(false);
-                cameras[1].SetActive(true);
-                break;
-            default:
-                Debug.Log("<color=#FF0000>ERROR AL ELEGIR NIVEL</color>");
-                break;
+            cameras[0].SetActive(true);
+            cameras[1].SetActive(false);         
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            cameras[0].SetActive(false);
+            cameras[1].SetActive(true);
+           
         }
     }
 }
